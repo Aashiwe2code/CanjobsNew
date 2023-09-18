5929444f0044bf07dabdcdd6a06a17a58135be67
shivaniwe2code <141002722+Sagarwe2code@users.noreply.github.com>
2023-09-18 16:27:28 +0530

    Latest build.

---
+++ precache-manifest.b178bf57fbf9b833463a0ad28f490ac2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e4ca09134391e370e58",
+    "url": "/static/css/main.05c530d2.chunk.css"
+  },
+  {
+    "revision": "3e4ca09134391e370e58",
+    "url": "/static/js/main.4436dc26.chunk.js"
+  },
+  {
+    "revision": "d378fe90b2328cf1390a",
+    "url": "/static/js/runtime~main.370a959e.js"
+  },
+  {
+    "revision": "72b9af5964f67ebf50ef",
+    "url": "/static/css/2.dffc477b.chunk.css"
+  },
+  {
+    "revision": "72b9af5964f67ebf50ef",
+    "url": "/static/js/2.a6117bfb.chunk.js"
+  },
+  {
+    "revision": "11213451287056a71ab7",
+    "url": "/static/js/3.f4f57a3b.chunk.js"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "ce38b86ea5173a6e60dc02a219a47094",
+    "url": "/static/media/verified.c0dd6ad7.png"
+  },
+  {
+    "revision": "35404cf4157e168cf29f602fd1cbb0cb",
+    "url": "/index.html"
+  }
+];
