379c9448d2f74b46b075c457ddf1ca57ba5d94d4
Aashi vyas <141002722+Aashi.we2code@users.noreply.github.com>
2023-11-27 14:23:11 +0530

    Latest build 2.22 27/11/23.

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5aaee756538cd616eb254b843bf4b75c.js"
+  "/precache-manifest.1e981bf3caa4c7b6dffc7cd6c978c60a.js"
 );
 
 workbox.clientsClaim();
