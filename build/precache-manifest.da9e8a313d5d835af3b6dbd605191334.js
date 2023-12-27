08b86d55b1a50f6411f72a29019c7f29c532acd2
Aashi vyas <141002722+Aashi.we2code@users.noreply.github.com>
2023-12-27 12:05:27 +0530

    Minor changes in the job table.

---
+++ build/precache-manifest.da9e8a313d5d835af3b6dbd605191334.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccbec84c0055811e3f49",
+    "url": "/static/css/main.79e8baa6.chunk.css"
+  },
+  {
+    "revision": "ccbec84c0055811e3f49",
+    "url": "/static/js/main.a2c0013e.chunk.js"
+  },
+  {
+    "revision": "2d2be85258a7f2628cb8",
+    "url": "/static/js/runtime~main.d1566087.js"
+  },
+  {
+    "revision": "38ab0c8d59884fd5e6e9",
+    "url": "/static/css/2.dffc477b.chunk.css"
+  },
+  {
+    "revision": "38ab0c8d59884fd5e6e9",
+    "url": "/static/js/2.926a60d5.chunk.js"
+  },
+  {
+    "revision": "74756de1ce4fdd488d68",
+    "url": "/static/js/3.f54a1483.chunk.js"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "ce38b86ea5173a6e60dc02a219a47094",
+    "url": "/static/media/verified.ce38b86e.png"
+  },
+  {
+    "revision": "b52c0575142e4acfa2e87c68e1a46e47",
+    "url": "/index.html"
+  }
+];
