773164bfce705588f5ae96b4d1ab8ac352a039d7
Aashi vyas <141002722+Aashi.we2code@users.noreply.github.com>
2024-01-31 18:09:42 +0530

    Latest push.

---
+++ build/precache-manifest.6e5d8b7d7472c821fed1cce260b75a4e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5e6a731b5089c082074",
+    "url": "/static/css/main.c8be17c6.chunk.css"
+  },
+  {
+    "revision": "c5e6a731b5089c082074",
+    "url": "/static/js/main.4a8d9a61.chunk.js"
+  },
+  {
+    "revision": "c4096eae2f002a4e621f",
+    "url": "/static/js/runtime~main.225b5b86.js"
+  },
+  {
+    "revision": "455e22875702c01d5a98",
+    "url": "/static/css/2.dffc477b.chunk.css"
+  },
+  {
+    "revision": "455e22875702c01d5a98",
+    "url": "/static/js/2.6954c163.chunk.js"
+  },
+  {
+    "revision": "ce10238580549370e777",
+    "url": "/static/js/3.9a234626.chunk.js"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "ce38b86ea5173a6e60dc02a219a47094",
+    "url": "/static/media/verified.ce38b86e.png"
+  },
+  {
+    "revision": "19b0da6bce36b2e72d21356a1eaf725c",
+    "url": "/index.html"
+  }
+];
