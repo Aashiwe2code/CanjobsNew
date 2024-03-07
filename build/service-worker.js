8d10681eaddd0ea7c8639709ed8c74e9c9031e5e
Aashi vyas <aashi.we2code@gmail.com>
2024-03-07 10:24:22 +0530

    Latest build 10.24.

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da0079625f4a3bb0cc9c9121ab12c07b.js"
+  "/precache-manifest.4b9db50b8575a7ed0c8e30af4c935329.js"
 );
 
 workbox.clientsClaim();
